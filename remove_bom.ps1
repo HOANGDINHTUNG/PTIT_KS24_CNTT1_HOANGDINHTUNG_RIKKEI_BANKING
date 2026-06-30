ce82ffa9bffd8bd8cb38cc56fb1900521d4d869d
unknown <mtung3365@gmail.com>
2026-06-30 15:02:44 +0700

    refactor

--- remove_bom.ps1
+++
@@ -1,9 +0,0 @@
-$files = Get-ChildItem -Path src -Filter *.java -Recurse
-foreach ($f in $files) {
-    if ($f -is [System.IO.FileInfo]) {
-        try {
-            $content = [System.IO.File]::ReadAllText($f.FullName)
-            [System.IO.File]::WriteAllText($f.FullName, $content, (New-Object System.Text.UTF8Encoding $False))
-        } catch {}
-    }
-}
